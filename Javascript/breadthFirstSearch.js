91ea236b269955a8b75a6f4242912a18220415c3
Sabi Kumar <skumar213@gmail.com>
2022-01-10 14:44:46 -0500

    added comments

--- Javascript/breadthFirstSearch.js
+++ Javascript/breadthFirstSearch.js
@@ -22,18 +22,28 @@ DFSPre(node-a, callback) -> A, B, D, E, C, F, G
 DFSPost(node-a, callback) -> D, E, B, F, G, C, A
 
 ------Solution------
+BFS
+Use a queue to keep track of each node, then use a while loop to go through each node. Since it's breadth-first, you process the current node and then add the children to the queue (in order so the first one added will be the first one looked at).
 
+DFSPre
+Process the current node first and then recursively call the same function on its children
 
-
-
+DFSPost
+Recursively call the function on the nodes children, then process the node.
 
 ------Time complexity------
-FOR ALL
+BFS
 O(N * M) - The function has to loop through all the nodes and the forEach method has to loop through the node's children
 
+DFSPre & DFSPost
+O(N) - The function has to go through all the nodes
+
 ------Space complexity------
-FOR ALL
-O(N) - the queue/stack will populate with the each node's children
+BFS
+O(N) - The queue will populate with the each node's children
+
+DFSPre & DFSPost
+O(N) - The call stack will use space due to it being recursive
 */
 
 //A,
@@ -47,27 +57,28 @@ const BFS = (node, callback) => {
 
     callback(currentNode.value);
 
-    currentNode.children.forEach(n => queue.unshift(n))
+    currentNode.children.forEach(n => queue.unshift(n));
   }
-}
-
+};
 
 const DFSPre = (node, callback) => {
-  callback(node.value)
+  callback(node.value);
 
   for (let child of node.children) {
-    DFSPre(child, callback)
+    DFSPre(child, callback);
   }
-
-}
+};
 
 const DFSPost = (node, callback) => {
   for (let child of node.children) {
-    DFSPost(child, callback)
+    DFSPost(child, callback);
   }
 
-  callback(node.value)
-}
+  callback(node.value);
+};
+
+
+
 
 
 
@@ -78,19 +89,19 @@ function node(value) {
     children: [],
   };
 }
-var a = node('a');
-var b = node('b');
-var c = node('c');
-var d = node('d');
-var e = node('e');
-var f = node('f');
-var g = node('g');
-var h = node('h');
-var i = node('i');
-var j = node('j');
-var k = node('k');
-var l = node('l');
-var m = node('m');
+var a = node("a");
+var b = node("b");
+var c = node("c");
+var d = node("d");
+var e = node("e");
+var f = node("f");
+var g = node("g");
+var h = node("h");
+var i = node("i");
+var j = node("j");
+var k = node("k");
+var l = node("l");
+var m = node("m");
 
 a.children.push(b, c, d);
 b.children.push(e);
@@ -99,16 +110,10 @@ c.children.push(f, g, h);
 h.children.push(m);
 d.children.push(i, j);
 
-
 const solutionCheck = input => {
-  console.log(input)
+  console.log(input);
 };
 
 // BFS(a, solutionCheck)
-DFSPre(a, solutionCheck);
+// DFSPre(a, solutionCheck);
 // DFSPost(a, solutionCheck);
-
-
-
-
-
