218461ac66dd1e0bc61bd491ba8cf2fbe08e7038
Sabi Kumar <skumar213@gmail.com>
2022-01-17 12:58:09 -0500

    first idea for deepestnode

--- Javascript/deepestNode.js
+++ Javascript/deepestNode.js
@@ -1,11 +1,28 @@
 /*
 ------Prompt------
-
+Given a binary tree, write a function that will return the node in the tree with greatest depth. You may assume there is a unique deepest node.
 
 ------Examples------
+tree1 =
+      30
+    25    35
+  20 26  32  40
+19
 
+deepestNode(tree1) -> 19
+
+tree2 =
+    A
+  B   C
+   D    E
+ F
+
+ deepestNode(tree2) -> F
 
 ------Solution------
+-check if the node has a left, if yes call function again with that as the head node
+-otherwise check the right, if yes call the function again with that as the head node
+-if there are no children, make that the deepest node, and change the counter to how many levels that node is
 
 
 ------Time complexity------
@@ -16,6 +33,35 @@ O() -
 */
 
 //------------Solution------------------
+const deepestNode = (node) => {
+  let deepestNode;
+  let deepestCount;
+  let counter = 0;
+
+  function findDeepest(node, counter) {
+    counter++;
+
+    if (!node.left && !node.right) {
+      deepest = node;
+      deepestCount = counter;
+
+      counter = 0
+    }
+
+    if (node.left){
+      findDeepest(node.left)
+    }
+
+    if (node.right) {
+      findDeepest(node.right)
+    }
+  }
+
+  deepestNode(node)
+  return deepestNode.val
+}
+
+
 
 
 
