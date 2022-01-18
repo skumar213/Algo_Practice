4cd2297f1bc2956604af94d46a664264b1f9dc67
Sabi Kumar <skumar213@gmail.com>
2022-01-18 12:01:33 -0500

    started working on subsetsum

---
+++ Javascript/subsetSum.js
@@ -0,0 +1,44 @@
+/*
+------Prompt------
+
+
+------Examples------
+
+
+------Solution------
+
+
+------Time complexity------
+O() -
+
+------Space complexity------
+O() -
+*/
+
+//------------Solution------------------
+
+
+
+
+
+//------------Solution Check------------------
+const inputs = [
+  [false, "[][(){}"],
+  [false, "({)}"],
+  [true, "({[]})"],
+  [true, "text ( is allowed ){rwwrwrrww [] ()}"],
+];
+
+const fn = hasBalancedBrackets;
+
+const solutionCheck = inputs => {
+  for (let input of inputs) {
+    const answer = input[0];
+    const arg1 = input[1];
+    const result = fn(arg1);
+
+    console.log("Answer:", answer, "|", "Result:", result);
+  }
+};
+
+solutionCheck(inputs);
