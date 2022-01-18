ca8db192ecc067d017262c525be782a3912076b3
Sabi Kumar <skumar213@gmail.com>
2022-01-18 12:59:39 -0500

    second approach subsetsum

--- Javascript/subsetSum.js
+++ Javascript/subsetSum.js
@@ -3,19 +3,24 @@
 Given a target sum and an array of positive integers, return true if any combination of numbers in the array can add to the target. Each number in the array may only be used once. Return false if the numbers cannot be used to add to the target sum.
 
 ------Examples------
-subsetSum(3, [1,2,3,4,5]) - True
-subsetSum(6, [1,2,3,4,5]) - True
-subsetSum(15, [1,2,3,4,5]) - True
-subsetSum(100, [1,2,3,4,5]) - False
+subsetSum(2, [1, 10, 5, 3]); // false
+subsetSum(10, [1, 10, 5, 3]); // true
+
+subsetSum(9, [1, 10, 5, 3]); // true
+
+subsetSum(19, [1, 10, 5, 3]); // true
+subsetSum(17, [1, 10, 5, 3]); // false
+
+
 
 ------Solution------
 Edge cases
 1) array is empty - return false
 2) target is 0 - return false, assuming only positive integers
+3) if array i [1] and target 1 - true
 
--loop through array and see if the first two numbers is the target, if not then the first three. Keep going until sum is greater than target.
--then call the function again but with length n-1 for the array input
--keep going until you either found the match (return true) or array is empty and no match (false)
+-loop through array and see if the first element and any other number equals the target. if not add the first two element together and see if any three equals the target. if this sum is greater than the target, skip it. If still no match, keep adding the on going sum with the next element and seeing if that subset equals the target.
+-if nothing matches, then change the array to n-1 and repeat the process until array is empty
 
 ------Time complexity------
 O() -
@@ -26,31 +31,23 @@ O() -
 
 //------------Solution------------------
 const subsetSum = (target, arr) => {
-  let sum = arr[0];
+  let sum = 0;
 
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 1; j < arr.legnth; i++) {
-      sum += arr[i];
+    sum += arr[i];
 
-      if (sum === target) {
-        return true;
-      } else if (sum > target) {
-        break;
-      }
+    for (let j = i + 1; arr.length; j++) {
+
     }
-
-
-
-
   }
 
-  if (arr.length) {
-    return subsetSum(target, arr.slice(1))
-  } else {
-    return false
-  }
 
-}
+
+};
+
+
+
+
 
 
 
