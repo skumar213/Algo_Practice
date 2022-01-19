6dc92ec791dcc7f64522e24cc2034a16770c461c
Sabi Kumar <skumar213@gmail.com>
2022-01-19 13:00:32 -0500

    finished comments subsetSum

--- Javascript/subsetSum.js
+++ Javascript/subsetSum.js
@@ -9,17 +9,7 @@ subsetSum(9, [1, 10, 5, 3]); // true
 subsetSum(19, [1, 10, 5, 3]); // true
 subsetSum(17, [1, 10, 5, 3]); // false
 
-
-
 ------Solution------
-Edge cases
-1) array is empty - return false
-2) target is 0 - return false, assuming only positive integers
-3) if array i [1] and target 1 - true
-
--loop through array and see if the first element and any other number equals the target. if not add the first two element together and see if any three equals the target. if this sum is greater than the target, skip it. If still no match, keep adding the on going sum with the next element and seeing if that subset equals the target.
--if nothing matches, then change the array to n-1 and repeat the process until array is empty
-
 //unoptimized solution
 Loop through the array and add the current number to the sum. If the sum is the target, return true. Else if the sum is greater than the target continue the loop with the next iteration. Otherwise, loop through the rest of the array adding the each number to the current sum individually and checking if it matches the target. If no match, add the next number to the sum and repeat the process. This will check for each combination of the first number in the array with all the other numbers. If there is no match, the function will be call again but without the first element of the array. If there is no match and the array length is 0, it will return false.
 
@@ -38,10 +28,18 @@ The sums array will grow as follows keeping track of all the previous sums.
 
 
 ------Time complexity------
-O(2 ^ N) -
+//unoptimized solution
+O(2 ^ N) - Have to go through all the subsets
+
+//optimized solution
+O(N * M) - Have to iterate through the input array and the array copy
 
 ------Space complexity------
-O(2 ^ N) -
+//unoptimized solution
+O(N) - The recursive call stack will grow to the size of the input array.
+
+//optimized solution
+O(2 ^ N) - The sums array will keep a copy of all subsets
 */
 
 //------------Solution------------------
@@ -71,7 +69,6 @@ const subsetSum = (target, arr) => {
   return false;
 };
 
-
 //optimized solution
 const optiSubsetSum = (target, arr) => {
   const sums = [0];
@@ -85,17 +82,15 @@ const optiSubsetSum = (target, arr) => {
       if (tmpSum === target) return true;
 
       sums.push(tmpSum);
-
-
     }
   }
 
   return false;
-}
+};
 
 //------------Solution Check------------------
 
-console.log('unoptimized')
+console.log("unoptimized");
 console.log(subsetSum(2, [1, 10, 5, 3])); // false
 console.log(subsetSum(10, [1, 10, 5, 3])); // true
 console.log(subsetSum(9, [1, 10, 5, 3])); // true
@@ -103,7 +98,7 @@ console.log(subsetSum(8, [1, 10, 5, 3])); // true
 console.log(subsetSum(19, [1, 10, 5, 3])); // true
 console.log(subsetSum(17, [1, 10, 5, 3])); // false
 
-console.log('optimized')
+console.log("optimized");
 console.log(optiSubsetSum(2, [1, 10, 5, 3])); // false
 console.log(optiSubsetSum(10, [1, 10, 5, 3])); // true
 console.log(optiSubsetSum(9, [1, 10, 5, 3])); // true
