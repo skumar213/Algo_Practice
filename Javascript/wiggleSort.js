0a22d62432e7a76ccbded18d51fa08acc2a6b67d
Sabi Kumar <skumar213@gmail.com>
2022-01-22 10:27:12 -0500

    added potential solution

--- Javascript/wiggleSort.js
+++ Javascript/wiggleSort.js
@@ -5,12 +5,25 @@ Given an integer array nums, reorder it such that nums[0] < nums[1] > nums[2] < 
 You may assume the input array always has a valid answer.
 
 ------Examples------
-wiggleSort([3,2,4,1]) = [1,2,3,4]
 wiggleSort([1]) = [1]
 wiggleSort([]) = [];
+wiggleSort([1,3,2,1]) = [1,3,1,2]
+wiggleSort([1,3,2,2,3,1]) = [2,3,1,3,1,2]
+
+[1,1,2,2,3,3]
+
+[2,3,1,3,1,2]
+
 
 
 ------Solution------
+Each 3 combo is a subset and the start and end of each combo is part of another subset (except for first and last numbers)
+
+Make an array with the same length as the input with values of null
+Sort input array
+Use a while loop (array includes null), then pop() (will be the current largest) and insert it into odd indexes until you hit the end, then it will loop again putting the current largest on even inputs. So it will take two loops
+
+
 
 
 ------Time complexity------
@@ -21,6 +34,11 @@ O() -
 */
 
 //------------Solution------------------
+const wiggleSort = (nums) => {
+
+}
+
+
 
 
 
@@ -47,3 +65,15 @@ const solutionCheck = inputs => {
 };
 
 solutionCheck(inputs);
+
+
+
+
+
+
+
+
+
+
+
+
