8999f2b86ca8a0fcfbf49345d43de4fa71d609a4
Sabi Kumar <skumar213@gmail.com>
2022-01-22 11:30:25 -0500

    working solution

--- Javascript/wiggleSort.js
+++ Javascript/wiggleSort.js
@@ -15,7 +15,6 @@ wiggleSort([1,3,2,2,3,1]) = [2,3,1,3,1,2]
 [2,3,1,3,1,2]
 
 
-
 ------Solution------
 Each 3 combo is a subset and the start and end of each combo is part of another subset (except for first and last numbers)
 
@@ -23,8 +22,11 @@ Make an array with the same length as the input with values of null
 Sort input array
 Use a while loop (array includes null), then pop() (will be the current largest) and insert it into odd indexes until you hit the end, then it will loop again putting the current largest on even inputs. So it will take two loops
 
+[1,1,2,1,2,2,1]
 
+[1,1,1,1,2,2,2]
 
+[1,0,1,0,1,0,1]
 
 ------Time complexity------
 O() -
@@ -34,46 +36,48 @@ O() -
 */
 
 //------------Solution------------------
-const wiggleSort = (nums) => {
+const wiggleSort1 = nums => {
+  if (nums.length <= 1) return nums;
 
-}
+  const numsCopy = nums.map(e => e);
+  numsCopy.sort((a, b) => a - b);
 
+  const indexStart = nums.length % 2 === 0 ? 1 : 0;
+  const secondLoopIndex = nums.length % 2 === 0 ? -2 : -1;
 
+  for (let i = indexStart; i < nums.length; i += 2) {
+    const currentLargestNum = indexStart === 1 ? numsCopy.pop() : numsCopy.shift();
+    nums[i] = currentLargestNum;
 
-
-
-
-
-//------------Solution Check------------------
-const inputs = [
-  [false, "[][(){}"],
-  [false, "({)}"],
-  [true, "({[]})"],
-  [true, "text ( is allowed ){rwwrwrrww [] ()}"],
-];
-
-const fn = hasBalancedBrackets;
-
-const solutionCheck = inputs => {
-  for (let input of inputs) {
-    const answer = input[0];
-    const arg1 = input[1];
-    const result = fn(arg1);
-
-    console.log("Answer:", answer, "|", "Result:", result);
+    if (i === nums.length - 1) i = secondLoopIndex;
   }
+
+  return nums;
 };
 
-solutionCheck(inputs);
 
 
+const wiggleSort = nums => {
+  if (nums.length <= 1) return nums;
 
+  const numsCopy = nums.map(e => e);
+  numsCopy.sort((a, b) => a - b);
 
+  const indexStart = nums.length % 2 === 0 ? 1 : 0;
+  const secondLoopIndex = nums.length % 2 === 0 ? -2 : -1;
 
+  for (let i = indexStart; i < nums.length; i += 2) {
+    const currentLargestNum = indexStart === 1 ? numsCopy.pop() : numsCopy.shift();
+    nums[i] = currentLargestNum;
 
+    if (i === nums.length - 1) i = secondLoopIndex;
+  }
 
+  return nums;
+};
 
+//
 
+//------------Solution Check------------------
 
-
-
+console.log(wiggleSort([1, 1, 2, 1, 2, 2, 1]));
