bf39a105518d319ba5a393a331f572ac0bc5a5fd
Sabi Kumar <skumar213@gmail.com>
2022-01-26 12:17:58 -0500

    started working on min bribes

---
+++ Javascript/minimumBribes.js
@@ -0,0 +1,51 @@
+/*
+------Prompt------
+It is New Year's Day and people are in line for the Wonderland rollercoaster ride. Each person wears a sticker indicating their initial position in the queue from 1 to n. Any person can bribe the person directly in front of them to swap positions, but they still wear their original sticker. One person can bribe at most two others.
+
+Determine the minimum number of bribes that took place to get to a given queue order. Print the number of bribes, or, if anyone has bribed more than two people, print Too chaotic.
+
+------Examples------
+-Inputs & Outputs
+input = an array of numbers that represent a position in line, order matters
+output = an array of numbers that represent 
+
+-Edge Cases
+
+------Solution------
+
+
+
+------Time complexity------
+O() -
+
+------Space complexity------
+O() -
+*/
+
+//------------Solution------------------
+//Test all inputs before testing
+
+
+
+
+//------------Solution Check------------------
+const inputs = [
+  [false, "[][(){}"],
+  [false, "({)}"],
+  [true, "({[]})"],
+  [true, "text ( is allowed ){rwwrwrrww [] ()}"],
+];
+
+const fn = hasBalancedBrackets;
+
+const solutionCheck = inputs => {
+  for (let input of inputs) {
+    const answer = input[0];
+    const arg1 = input[1];
+    const result = fn(arg1);
+
+    console.log("Answer:", answer, "|", "Result:", result);
+  }
+};
+
+solutionCheck(inputs);
