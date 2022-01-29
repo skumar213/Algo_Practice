d8026c54c4a4f2d3ed31b0b3fad457c97e8a2b5f
Sabi Kumar <skumar213@gmail.com>
2022-01-29 14:35:15 -0500

    updated explination for count bits

--- Javascript/countBits.js
+++ Javascript/countBits.js
@@ -3,7 +3,6 @@
 Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
 
 
-
 ------Examples------
 -Inputs & Outputs
 inputs = an integet
@@ -16,12 +15,14 @@ n will always be positive
 
 -Examples
 countBits(2) = [0,1,1]
-countBits 5 = [0,1,1,2,1,2]
+countBits(5) = [0,1,1,2,1,2]
 
 ------Solution------
 For loop from 0 to n
   for each number convert it to binary and count the 1s
   then push up to the ans array
+
+Use a helper function to count the bits using the bitwise operators. It uses a trick to count the number of ones. The &= operator will compare the binary equivalent of two numbers (n and n-1 )by checking to the two and returning a 1 for which the corresponding bits of both operands are 1's. Then this is converted back to its decimal form and becomes x. This works because it converts the least-significant (right most) bit of the current number to it's opposite. You can only do this for the number of total one's per digit.
 
 
 ------Time complexity------
@@ -34,7 +35,7 @@ O(1) - The output array doesn't count towards the space compexity.
 //------------Solution------------------
 //Test all inputs before testing
 
-const countBits = (n) => {
+const countBits = n => {
   const ans = [];
 
   for (let i = 0; i <= n; i++) {
@@ -43,27 +44,18 @@ const countBits = (n) => {
     ans.push(bitCount);
   }
 
-  return ans
-}
+  return ans;
+};
 
-
-
-const getCount = (x) => {
+const getCount = x => {
   let count;
   for (count = 0; x !== 0; count++) {
-    console.log(x)
-    x &= x - 1
-    console.log(x, count)
+    x &= x - 1;
   }
 
   return count;
-}
-//As input increases the number of iterations never passes 32 since its checking 32-bit integers. Worst case it would be all 1's.
-
-
-
-
+};
 
 //------------Solution Check------------------
-console.log(optiCount(4))
-
+console.log(countBits(2)); // [0,1,1]
+console.log(countBits(5)); // [0,1,1,2,1,2]
