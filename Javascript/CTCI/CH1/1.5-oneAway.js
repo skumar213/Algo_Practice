d56cab45eb65d211ea0cbc399c13bb4fd1903514
Sabi Kumar <skumar213@gmail.com>
2022-03-22 14:06:24 -0400

    finished CTCI 1.5

---
+++ Javascript/CTCI/CH1/1.5-oneAway.js
@@ -0,0 +1,43 @@
+function oneAway(w1, w2) {
+  const sameLength = w1.length === w2.length ? true : false;
+
+  if (w1.length < w2.length) {
+    const tmp = w1;
+    w1 = w2;
+    w2 = w1;
+  }
+
+  let p1 = -1;
+  let p2 = 0;
+  let count = 0;
+
+  while (p1 < w1.length && p2 < w2.length) {
+    p1++;
+
+    if (w1[p1] !== w2[p2]) {
+      count++;
+      if (count > 1) return false;
+
+      //needed when for the 'replace' condition
+      if (!sameLength) {
+        continue;
+      }
+    }
+
+    p2++;
+  }
+
+  return true;
+}
+
+//Tests
+console.log(oneAway("pale", "ple")); // true - remove
+console.log(oneAway("pale", "pales")); // true - insert
+console.log(oneAway("pale", "bale")); // true - replace
+console.log(oneAway("pale", "bake")); // false
+
+/*
+-------------Space and Time Complexity-----------
+Space: O(n) - a copy of the shorter array is made if w1 < w2
+Time: O(n) - where n is the length of the shorter word
+*/
