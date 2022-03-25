2cc90a35758db60b9a2ef69996b5770a66216547
Sabi Kumar <skumar213@gmail.com>
2022-03-25 16:49:50 -0400

    kDeepFromEnd algo

---
+++ Javascript/Misc/kDeepFromEnd.js
@@ -0,0 +1,46 @@
+//Finds the kth element from the end in a linked list
+
+function kDeepFromEnd(head, k) {
+  let head2 = head;
+
+  //get the k - 1 element from the head
+  for (let i = 1; i < k; i++) {
+    head2 = head2.next; //2
+    if (!head2) return "too short";
+  }
+
+  while (head2.next) {
+    head = head.next;
+    head2 = head2.next;
+  }
+
+  return head.value;
+}
+
+//Test
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+let one = new Node(1);
+let two = new Node(2);
+let three = new Node(3);
+let four = new Node(4);
+let five = new Node(5);
+let six = new Node(6);
+
+one.next = two;
+two.next = three;
+three.next = four;
+four.next = five;
+five.next = six;
+
+console.log(kDeepFromEnd(one, 3)); //4
+console.log(kDeepFromEnd(one, 13)); //too short
+console.log(kDeepFromEnd(one, 6)); //1
+
+//Time: O(n) - goes through full array
+//Space: O(1) - no space is used
