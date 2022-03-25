b75cf66d5dbfd53792e60637e55f5c3742ed59f9
Sabi Kumar <skumar213@gmail.com>
2022-03-25 10:52:22 -0400

    added insertionsort

---
+++ Javascript/Misc/sorting/insertionSort.js
@@ -0,0 +1,26 @@
+const arrSwap = (arr, oneIdx, twoIdx) => {
+  const tmp = arr[oneIdx];
+  arr[oneIdx] = arr[twoIdx];
+  arr[twoIdx] = tmp;
+};
+
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let j = i;
+
+    while (j > 0 && arr[j] < arr[j - 1]) {
+      arrSwap(arr, j, j - 1);
+      j--;
+    }
+  }
+
+  return arr;
+}
+
+//Test
+console.log(insertionSort([5, 4, 3, 2, 1]));
+
+//Time: O(n^2) - have to go through the array again for each element
+//Space: O(1) - constant space is used
+
+//How it works: it checks paris at a time and takes the element at j and keeps bringing it back until its no longer smaller than the one before it
