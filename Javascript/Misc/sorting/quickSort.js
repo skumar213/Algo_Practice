3ad8898f6da671e24ce93e728ca4ebe25ba6b006
Sabi Kumar <skumar213@gmail.com>
2022-03-28 17:12:47 -0400

    added optimized quicksort

--- Javascript/Misc/sorting/quickSort.js
+++ Javascript/Misc/sorting/quickSort.js
@@ -6,7 +6,25 @@ const arrSwap = (arr, oneIdx, twoIdx) => {
   arr[twoIdx] = tmp;
 };
 
-function quickSort(arr, startIdx, endIdx) {
+function quickSort(arr, startIdx, endIdx, isStart) {
+  //randomizes array on input to guarantee the input is random and that we get optimal runtime
+  if (isStart) {
+    for (let i = 0; i < arr.length; i++) {
+      let ranNum = Math.floor(
+        Math.random() * (endIdx + 1 - startIdx) + startIdx
+      );
+      if (ranNum === i) {
+        if (i !== arr.length - 1) {
+          ranNum++;
+        } else {
+          ranNum--;
+        }
+      }
+      arrSwap(arr, i, ranNum);
+    }
+    console.log(arr);
+  }
+
   if (startIdx < endIdx) {
     let pivot = partition(arr, startIdx, endIdx);
     quickSort(arr, startIdx, pivot - 1);
@@ -20,8 +38,8 @@ function partition(arr, startIdx, endIdx) {
   let pivot = endIdx;
   let divider = startIdx;
 
-  for (let i = startIdx; i < endIdx; i++) {
-    if (arr[i] < arr[pivot]) {
+  for (let i = startIdx; i <= endIdx; i++) {
+    if (arr[i] < arr[pivot] && i !== pivot) {
       arrSwap(arr, i, divider);
       divider++;
     }
@@ -33,9 +51,10 @@ function partition(arr, startIdx, endIdx) {
 }
 
 //Test
-const input = [4, 5, 1, 3, 2];
-console.log(quickSort(input, 0, input.length - 1));
+const input = [4, 5, 2, 3, 1];
+console.log(quickSort(input, 0, input.length - 1, true));
 
+//Unoptimized
 //Time: O(n^2) - in the worst case the depth of the recursive calls will be n and it takes linear time to perfrom the work on each level giving it n^2.
 //Space: O(n) - this is due to the recursive calls and in the worst case it will have to go n deep in one direction of an unbalanced tree.
 
@@ -49,3 +68,7 @@ console.log(quickSort(input, 0, input.length - 1));
 
 items without brackets is the answer -> 1,2,3,4,5
 */
+
+//Optimized
+//Time: O(nlogn) - since we randomize the input we raise the probabilty of not getting a bad pivot on each turn. this allows the algorithm to have an average case of logn calls instead of n. thus making the runtime nlogn
+//Space: O(logn) - since the recursive calls will be logn on average with the randomized input the space used will also be logn
