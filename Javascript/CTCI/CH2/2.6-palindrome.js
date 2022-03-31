124699500112507b8df12a7b7ec4b6f16af52986
Sabi Kumar <skumar213@gmail.com>
2022-03-31 11:06:09 -0400

    updated code to keep linked list in original form

--- Javascript/CTCI/CH2/2.6-palindrome.js
+++ Javascript/CTCI/CH2/2.6-palindrome.js
@@ -1,39 +1,41 @@
 function listPal(node) {
   let head = node;
   let tail = null;
-  let p1 = 1 //first element
-  let p2 = 0 //last element
+  let p1 = 1; //first element
+  let p2 = 0; //last element
 
-  while(node) {
+  while (node) {
     node.prev = tail;
     tail = node;
-    p2++
+    p2++;
     node = node.next;
   }
 
-  while(p1 < p2) {
+  while (p1 < p2) {
     if (head.value.toLowerCase() !== tail.value.toLowerCase()) return false;
 
+    delete head.prev; //to keep the linked-list in its original form
     head = head.next;
     tail = tail.prev;
+    delete tail.next.prev; //to keep the linked-list in its original form
 
-    p1++
-    p2--
+    p1++;
+    p2--;
   }
 
   return true;
 }
 
 //Tests
-class Node{
-  constructor(value){
+class Node {
+  constructor(value) {
     this.value = value;
     this.next = null;
   }
 }
 
 //checks odd word count
-const oddCheck = (bool) => {
+const oddCheck = bool => {
   const r = new Node("r");
   const a = new Node("a");
   const c = new Node("c");
@@ -47,37 +49,36 @@ const oddCheck = (bool) => {
   c.next = e;
   e.next = c2;
   c2.next = a2;
-  a2.next = bool ? r2 : null
+  a2.next = bool ? r2 : null;
 
   console.log(listPal(r));
-}
+};
 
 //checks even word count
-const evenCheck = (bool) => {
+const evenCheck = bool => {
   const a = new Node("a");
   const a2 = new Node("a");
   const a3 = new Node("a");
-  const a4 = bool ? new Node("a") : new Node("b")
+  const a4 = bool ? new Node("a") : new Node("b");
 
   a.next = a2;
   a2.next = a3;
-  a3.next = a4
+  a3.next = a4;
 
-  console.log(listPal(a))
-}
+  console.log(listPal(a));
+};
 
 //checks for single node
 const singleCheck = () => {
   const a = new Node("a");
 
-  console.log(listPal(a))
-}
+  console.log(listPal(a));
+};
 
 //oddCheck and evenCheck takes a single argument that indicates if the input should be a palindrome or not in order to check both resutls
 oddCheck(true);
 evenCheck(true);
 singleCheck();
-
 
 //Time: O(n) - have to loop through the whole list to get the length and set the tail pointers
 //Space: O(1) - no extra space is used
