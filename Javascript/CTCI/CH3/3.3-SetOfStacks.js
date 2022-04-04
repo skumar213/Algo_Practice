28444f29eb99ee53df087ff888f451b260fd4054
Sabi Kumar <skumar213@gmail.com>
2022-04-04 10:24:51 -0400

    added rollover

--- Javascript/CTCI/CH3/3.3-SetOfStacks.js
+++ Javascript/CTCI/CH3/3.3-SetOfStacks.js
@@ -41,20 +41,39 @@ class SetOfStacks {
     const rmItem = this.stack[index].pop();
 
     if (this.stack[index].length === 0) this.stack.splice(index, 1);
+    else {
+      const rollOverItems = [];
+
+      while (this.stack[index]) {
+        const item = this.stack[index].shift();
+        rollOverItems.push(item);
+
+        if (this.stack[index].length === 0) this.stack.splice(index, 1);
+      }
+
+      // console.log(rollOverItems)
+      // return
+      while (rollOverItems.length) {
+        const item = rollOverItems.shift();
+
+        this.push(item);
+      }
+    }
+
     return rmItem;
   }
 }
 
 const stack = new SetOfStacks(2);
 
-stack.push(1);
-stack.push(2);
-
-stack.push(3);
-stack.push(4);
-
-stack.push(5);
 stack.push(6);
+stack.push(5);
+
+stack.push(4);
+stack.push(3);
+
+stack.push(2);
+stack.push(1);
 
 stack.pop();
 // stack.pop();
@@ -63,8 +82,8 @@ stack.pop();
 // stack.pop();
 // stack.pop();
 
-stack.popAt(1);
-// stack.popAt(1);
+stack.popAt(0);
+// stack.popAt(0);
 
 console.log(stack.stack);
 console.log(stack.peek());
