db8adbe6cba7baf83f9f666fbc0ab39ba9014eda
Sabi Kumar <skumar213@gmail.com>
2022-04-18 16:28:22 -0400

    added graph class

--- Javascript/CTCI/CH4/4.1-routeBetweenNodes.js
+++ Javascript/CTCI/CH4/4.1-routeBetweenNodes.js
@@ -1,3 +1,28 @@
+function pathFinder(graph, n1, n2) {
+  //add visited property
+  for (let node of graph.nodes) {
+    const queue = [node];
+
+    while (queue.length) {
+      const currentNode = queue.pop();
+      currentNode.visited = false;
+
+      for (let child of currentNode.children) {
+        if (child.visited !== false) queue.push(child);
+      }
+    }
+  }
+
+  //check n1 -> n2
+  if (isPath(n1, n2)) return true;
+
+  //clear visited property
+  clearVisited(n1);
+
+  //check n2 - n1
+  return isPath(n2, n1);
+}
+
 function isPath(n1, n2) {
   const queue = [n1];
 
@@ -15,7 +40,27 @@ function isPath(n1, n2) {
   return false;
 }
 
+function clearVisited(n1) {
+  const queue = [n1];
+
+  while (queue.length) {
+    const node = queue.pop();
+
+    node.visited = false;
+
+    for (let child of node.children) {
+      if (child.visited) queue.unshift(child);
+    }
+  }
+}
+
 //Tests
+class Graph {
+  constructor() {
+    this.nodes = [];
+  }
+}
+
 class Node {
   constructor(value) {
     this.value = value;
@@ -36,8 +81,13 @@ two.children.push(three);
 three.children.push(...[one, four]);
 five.children.push(four);
 
-console.log(isPath(zero, three)); //true
-// console.log(isPath(zero, five)); //false
+const graph = new Graph();
+graph.nodes.push(...[zero, five]);
+
+console.log(pathFinder(graph, zero, three)); //true
+console.log(pathFinder(graph, three, zero)); //true
+console.log(pathFinder(graph, zero, five)); //false
+console.log(pathFinder(graph, five, zero)); //false
 
 //Time: O(n) - need to visit each node
 //Space: O(1) - no extra space is used
