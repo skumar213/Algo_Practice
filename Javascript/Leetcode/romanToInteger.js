7c3503bcfefbeea983e0c58a54c1fe4d57e000e0
Sabi Kumar <skumar213@gmail.com>
2022-05-04 13:03:28 -0400

    formatting

--- Javascript/Leetcode/romanToInteger.js
+++ Javascript/Leetcode/romanToInteger.js
@@ -1,25 +1,25 @@
 //Link: https://leetcode.com/problems/roman-to-integer/
 
-var romanToInt = function(s) {
+var romanToInt = function (s) {
   let sum = 0;
 
   const convert = {
-      'I': 1,
-      'V': 5,
-      'X': 10,
-      'L': 50,
-      'C': 100,
-      'D': 500,
-      'M': 1000
-  }
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
 
   for (let i = 0; i < s.length; i++) {
-      if (i + 1 < s.length && convert[s[i]] < convert[s[i+1]]) {
-          sum += (convert[s[i+1]] - convert[s[i]])
-          i++
-      } else {
-          sum += convert[s[i]]
-      }
+    if (i + 1 < s.length && convert[s[i]] < convert[s[i + 1]]) {
+      sum += convert[s[i + 1]] - convert[s[i]];
+      i++;
+    } else {
+      sum += convert[s[i]];
+    }
   }
 
   return sum;
