6b885ccedcda299ea6c15eb0d3d9e83d7d07fbfc
Sabi Kumar <skumar213@gmail.com>
2022-06-17 13:37:08 -0400

    finished buy and sell stock

---
+++ Javascript/Blind75/Arrays/bestTimeToBuyAndSellStock.js
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+
+var maxProfit = function(prices) {
+  let maxProfit = 0
+  let minPrice = Infinity
+
+  for (let i = 0; i < prices.length; i++) {
+      const currentPrice = prices[i]
+
+      if (currentPrice < minPrice) {
+          minPrice = currentPrice
+      } else if (currentPrice - minPrice > maxProfit) {
+          maxProfit = currentPrice - minPrice
+      }
+  }
+
+  return maxProfit
+};
+
+//Space & Time
+//Time: O(n) - makes one pass through array
+//Space: O(1) - no extra space is used
+
+/*
+Trick/Pattern
+
+1) Brute force is n^2 with a double for loop. Can do it faster by having a pointer for the lowest number and then finding the higher number after it.
+2) You iterate through while keep track of the lowest number and then keep finding the different profits and keep track of the highest one
+
+This works because the highest profit will always occur after the lowest number, since the goal here is to buy low and then sell high. That's why you keep changing the lowest number and comparing profits to the current max profit
+*/
