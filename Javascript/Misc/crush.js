c71f1926b4255cd147fc3b7f7c1cac07422b7aa9
Sabi Kumar <skumar213@gmail.com>
2022-06-26 16:17:09 -0400

    fixed solution

--- Javascript/Misc/crush.js
+++ Javascript/Misc/crush.js
@@ -1,39 +1,28 @@
 function crush(str) {
-  if (uniqueCheck(str) || str.length < 3) return str;
+  if (str.length < 3) return str;
 
-  const count = countStr(str);
+  const stack = [str[0], str[1]];
 
-  if (count >= 3) return crush(str.slice(count));
+  for (let i = 2; i < str.length; i++) {
+    const topOne = stack[stack.length - 1];
+    const topTwo = stack[stack.length - 2];
 
-  const newStr = str.slice(0, count) + crush(str.slice(count));
-  return crush(newStr);
-}
-
-function uniqueCheck(str) {
-  const obj = {};
-
-  for (let char of str) {
-    if (!(char in obj)) obj[char] = true;
-    else return false;
+    if (topOne === str[i] && topTwo === str[i] && str[i] !== str[i + 1]) {
+      while (stack[stack.length - 1] === str[i]) {
+        stack.pop();
+      }
+    } else {
+      stack.push(str[i]);
+    }
   }
 
-  return true;
-}
-
-function countStr(str) {
-  let count = 1;
-  for (let i = 1; i < str.length; i++) {
-    if (str[0] === str[i]) count++;
-    else break;
-  }
-
-  return count;
+  return stack.join("");
 }
 
 const input1 = "aaabbbc"; //c
 const input2 = "aabbbacd"; //cd
 const input3 = "aabbccddeeedcba"; // ""
-const input4 = "aaabbbacd"; // acd
+const input4 = "aaabbbbacd"; // acd
 const input5 = "abcdddf"; // abcf
 
 console.log(crush(input1));
@@ -41,26 +30,3 @@ console.log(crush(input2));
 console.log(crush(input3));
 console.log(crush(input4));
 console.log(crush(input5));
-
-//Below is the iterative solution
-
-function crush(str) {
-  if (str.length < 3) return str
-
-  const stack = [str[0], str[1]]
-
-  for (let i = 2; i < str.length; i++) {
-    const topOne = stack[stack.length - 1];
-    const topTwo = stack[stack.length - 2];
-
-    if ((topOne === str[i] && topTwo === str[i]) && str[i] !== str[i + 1]){
-      while (stack[stack.length - 1] === str[i]) {
-        stack.pop();
-      }
-    } else {
-      stack.push(str[i])
-    }
-  }
-
-  return stack.join("")
-}
